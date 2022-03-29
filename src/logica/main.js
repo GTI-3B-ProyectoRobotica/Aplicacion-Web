e652e364a933fe57e9bb8855e5b472a97a741cea
Juan <tostyfis360@gmail.com>
2022-03-29 12:20:01 +0200

    Creado HTML y javaScript 29/03/2022

---
+++ src/logica/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', event => {
+    console.log("entro en la pagina")
+    
+    document.getElementById("btn_con").addEventListener("click", connect)
+    //document.getElementById("btn_dis").addEventListener("click", disconnect)
+
+    data = {
+        // ros connection
+        ros: null,
+        rosbridge_address: 'ws://127.0.0.1:9090/',
+        connected: false,
+    }
+
+    function connect(){
+	      console.log("Clic en connect")
+	
+	      data.ros = new ROSLIB.Ros({
+                url: data.rosbridge_address
+        })
+
+        // Define callbacks
+        data.ros.on("connection", () => {
+            data.connected = true
+            console.log("Conexion con ROSBridge correcta")
+        })
+        data.ros.on("error", (error) => {
+            console.log("Se ha producido algun error mientras se intentaba realizar la conexion")
+            console.log(error)
+        })
+        data.ros.on("close", () => {
+            data.connected = false
+            console.log("Conexion con ROSBridge cerrada")	    	 
+        })
+    }
+
+    function disconnect(){
+	      data.ros.close()        
+	      data.connected = false
+        console.log('Clic en botón de desconexión')
+    }    
+    
+});
